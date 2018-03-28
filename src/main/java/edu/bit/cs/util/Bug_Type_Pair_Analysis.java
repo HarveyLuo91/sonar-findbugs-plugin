f9d8dbe4954117ed376b8e837e302b7e1f2bcd4f
aminu241 <aminu241@rocketmail.com>
2018-03-28 12:29:50 +0800

    Added new files to Assesment and moe

---
+++ src/main/java/edu/bit/cs/util/Bug_Type_Pair_Analysis.java
@@ -0,0 +1,58 @@
+package edu.bit.cs.util;
+
+public class Bug_Type_Pair_Analysis {
+
+    public  int F_npe = 0;
+    public  int F_rl = 0;
+    public  int F_sync = 0;
+    public  int F_injc = 0;
+    public  int F_xss = 0;
+    public  int F_inhrit = 0;
+    public  int F_other = 0;
+
+    public  int J_npe = 0;
+    public  int J_sync = 0;
+    public  int J_inhrit = 0;
+    public  int J_other = 0;
+
+    public  int I_npe = 0;
+    public  int I_rl = 0;
+    public  int I_sync = 0;
+    public  int I_other = 0;
+
+    public  int J_I_npe = 0;
+    public  int J_I_sync = 0;
+    public  int J_I_other = 0;
+
+    public  int B_xss = 0; //BIT
+    public  int B_rl = 0;
+    public  int B_injc = 0;
+    public  int B_other = 0;
+
+    public  int F_J_npe = 0;
+    public  int F_J_sync = 0;
+    public  int F_J_inhrit = 0;
+    public  int F_J_other = 0;
+
+    public  int F_I_npe = 0;
+    public  int F_I_rl = 0;
+    public  int F_I_sync = 0;
+    public  int F_I_other = 0;
+
+    public int F_B_xss = 0;
+    public int F_B_rl = 0;
+    public int F_B_injc = 0;
+    public int F_B_other = 0;
+
+    public int F_J_I_npe = 0;
+    public int F_J_I_sync = 0;
+    public  int F_I_J_other = 0;
+
+
+
+
+
+
+
+
+}
