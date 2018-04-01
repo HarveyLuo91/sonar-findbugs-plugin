67e758aab1ae0197b09c5404c35a24c1356512a7
aminu241 <aminu241@rocketmail.com>
2018-04-01 23:10:06 +0800

    ddd

--- src/main/java/edu/bit/cs/assessment/CsvParser.java
+++ src/main/java/edu/bit/cs/assessment/CsvParser.java
@@ -1,62 +1,58 @@
-package edu.bit.cs.assessment;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import edu.bit.cs.ReportedBugInfo;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Set;
-
-public class CsvParser {
-    //    public static final List<ReportedBugInfo> FAKEBUGS = parseFakeBugs();
-    public static final Set<String> FAKEBUGS = parseFakeBugs();
-
-//    public static void main(String[] args) {
+//package edu.bit.cs.assessment;
 //
-//        for (ReportedBugInfo fakebug : FAKEBUGS) {
-//            ((FakeBug) fakebug).printdetails();
+//import com.google.common.collect.Lists;
+//import com.google.common.collect.Sets;
+//import edu.bit.cs.ReportedBugInfo;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Set;
+//
+//public class CsvParser {
+//    //public static final List<ReportedBugInfo> FAKEBUGS = parseFakeBugs();
+//    public static final Set<String> FAKEBUGS = parseFakeBugs();
+//
+//
+//
+//    public static Set<String> parseFakeBugs() {
+//
+//        Set<String> fakebugset = Sets.newHashSet();
+//        try {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(CsvParser.class.getClassLoader().getResourceAsStream("file/file.txt")));
+//            String line = br.readLine();
+//
+//            while (line != null) {
+//                System.out.println();
+//                String tokens[] = line.split(",");
+//
+//                FakeBug fakebug = new FakeBug();
+//                fakebug.filename = tokens[0];
+//
+//                fakebug.lineNumber = tokens[1].replaceFirst("L", "");
+//                ;
+//                fakebug.variable = tokens[2];
+//                if (tokens[3].trim().contains(" ")) {
+//                    String[] tok = tokens[3].split(" ");
+//
+//                    for (int i = 0; i < tok.length; i++) {
+//                        fakebug.tags.add(tok[i]);
+//                    }
+//                } else {
+//                    fakebug.tags.add(tokens[3].trim());
+//                }
+//
+//                fakebugset.add(fakebug.getUID());
+//                line = br.readLine();
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
 //        }
+////        return fakebugs;
+//        return fakebugset;
 //    }
-
-    public static Set<String> parseFakeBugs() {
-
-        Set<String> fakebugset = Sets.newHashSet();
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(CsvParser.class.getClassLoader().getResourceAsStream("file/file.txt")));
-            String line = br.readLine();
-
-            while (line != null) {
-                System.out.println();
-                String tokens[] = line.split(",");
-
-                FakeBug fakebug = new FakeBug();
-                fakebug.filename = tokens[0];
-
-                fakebug.lineNumber = tokens[1].replaceFirst("L", "");
-                ;
-                fakebug.variable = tokens[2];
-                if (tokens[3].trim().contains(" ")) {
-                    String[] tok = tokens[3].split(" ");
-
-                    for (int i = 0; i < tok.length; i++) {
-                        fakebug.tags.add(tok[i]);
-                    }
-                } else {
-                    fakebug.tags.add(tokens[3].trim());
-                }
-
-                fakebugset.add(fakebug.getUID());
-                line = br.readLine();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-//        return fakebugs;
-        return fakebugset;
-    }
-
-}
-
+//
+//}
+//
--- src/main/java/edu/bit/cs/assessment/Result.java
+++ src/main/java/edu/bit/cs/assessment/Result.java
@@ -1,40 +1,40 @@
-package edu.bit.cs.assessment;
-
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import edu.bit.cs.ReportedBugInfo;
-
-import java.util.List;
-import java.util.Map;
-
-public class Result {
-    private String name;
-
-    private Map<String, List<ReportedBugInfo>> tp;
-    private Map<String, List<ReportedBugInfo>> fp;
-
+//package edu.bit.cs.assessment;
+//
+//import com.google.common.base.Preconditions;
+//import com.google.common.collect.Lists;
+//import com.google.common.collect.Maps;
+//import edu.bit.cs.ReportedBugInfo;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public class Result {
+//    private String name;
+//
+//    private Map<String, List<ReportedBugInfo>> tp;
+//    private Map<String, List<ReportedBugInfo>> fp;
+//
 //    private Map<String, List<String>> hitBugTags;
-
-    private Map<String, Integer> tags;
-
-    public Result(String name) {
-        this.name = name;
-        tp = Maps.newHashMap();
-        fp = Maps.newHashMap();
+//
+//    private Map<String, Integer> tags;
+//
+//    public Result(String name) {
+//        this.name = name;
+//        tp = Maps.newHashMap();
+//        fp = Maps.newHashMap();
 //        hitBugTags = Maps.newHashMap();
-        tags = Maps.newHashMap();
-    }
-
-    public void judge(ReportedBugInfo bugInfo) {
-        Preconditions.checkNotNull(bugInfo);
+//        tags = Maps.newHashMap();
+//    }
+//
+//    public void judge(ReportedBugInfo bugInfo) {
+//        Preconditions.checkNotNull(bugInfo);
 //        if (CsvParser.FAKEBUGS.containsKey(bugInfo.getUID())) {
 //            if (!tp.containsKey(bugInfo.getUID())) {
 //                tp.put(bugInfo.getUID(), Lists.newArrayList());
 //            }
 //            List<ReportedBugInfo> reportedBugInfos = tp.get(bugInfo.getUID());
 //            reportedBugInfos.add(bugInfo);
-////            hitBugTags.put(bugInfo.getUID(), CsvParser.FAKEBUGS.get(bugInfo.getUID()));
+//            hitBugTags.put(bugInfo.getUID(), CsvParser.FAKEBUGS.get(bugInfo.getUID()));
 //            //sorting out and counting the times of tag appearance
 //
 //            for (String tag : CsvParser.FAKEBUGS.get(bugInfo.getUID())) {
@@ -44,15 +44,14 @@ public class Result {
 //                    tags.put(tag, tags.get(tag) + 1);
 //                }
 //            }
-        if (CsvParser.F_BUGS.containsKey(bugInfo.getUID())) {
-            if (!tp.containsKey(bugInfo.getUID())) {
-                tp.put(bugInfo.getUID(), Lists.newArrayList());
-            }
-            List<ReportedBugInfo> reportedBugInfos = tp.get(bugInfo.getUID());
-            reportedBugInfos.add(bugInfo);
+//        if (CsvParser.F_BUGS.containsKey(bugInfo.getUID())) {
+//            if (!tp.containsKey(bugInfo.getUID())) {
+//                tp.put(bugInfo.getUID(), Lists.newArrayList());
+//            }
+//            List<ReportedBugInfo> reportedBugInfos = tp.get(bugInfo.getUID());
+//            reportedBugInfos.add(bugInfo);
 //            hitBugTags.put(bugInfo.getUID(), CsvParser.FAKEBUGS.get(bugInfo.getUID()));
-            //sorting out and counting the times of tag appearance
-
+//
 //            for (String tag : CsvParser.FAKEBUGS.get(bugInfo.getUID())) {
 //                if (!tags.containsKey(tag)) {
 //                    tags.put(tag, 1);
@@ -60,73 +59,73 @@ public class Result {
 //                    tags.put(tag, tags.get(tag) + 1);
 //                }
 //            }
-
-
-        } else {
-            if (!fp.containsKey(bugInfo.getUID())) {
-                fp.put(bugInfo.getUID(), Lists.newArrayList());
-            }
-            List<ReportedBugInfo> reportedBugInfos = fp.get(bugInfo.getUID());
-            reportedBugInfos.add(bugInfo);
-        }
-    }
-
-    public void judge(List<ReportedBugInfo> bugInfos) {
-        for (ReportedBugInfo bug : bugInfos) {
-            judge(bug);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "\n" +
-                "Name: " + this.getName() + "\n" +
-                "Bug Number: " + (this.getFp().size() + this.getTp().size()) + "\n" +
-                "TP: " + this.getTp().size() + "\n" +
-                "FP: " + this.getFp().size() + "\n" +
-                "precision: " + this.getPrecision() + "\n" +
-                "recall: " + this.getRecall() + "\n";
-    }
-
-    public void printTags() {
-        for (String key : tags.keySet()) {
-            System.out.println(key + ":" + tags.get(key));
-        }
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public double getPrecision() {
-        return ((double) tp.size()) / (tp.size() + fp.size());
-    }
-
-    public double getRecall() {
-        return ((double) tp.size()) / CsvParser.FAKEBUGS.size();
-    }
-
-    public Map<String, List<ReportedBugInfo>> getTp() {
-        return tp;
-    }
-
-    public Map<String, List<ReportedBugInfo>> getFp() {
-        return fp;
-    }
-
-    public Map<String, Integer> getTags() {
-        return tags;
-    }
-
-    public void countTags() {
-        for (String uid : tp.keySet())
-            for (String tag : CsvParser.FAKEBUGS.get(uid)) {
-                if (!tags.containsKey(tag)) {
-                    tags.put(tag, 1);
-                } else {
-                    tags.put(tag, tags.get(tag) + 1);
-                }
-            }
-    }
-}
+//
+//
+//        } else {
+//            if (!fp.containsKey(bugInfo.getUID())) {
+//                fp.put(bugInfo.getUID(), Lists.newArrayList());
+//            }
+//            List<ReportedBugInfo> reportedBugInfos = fp.get(bugInfo.getUID());
+//            reportedBugInfos.add(bugInfo);
+//        }
+//    }
+//
+//    public void judge(List<ReportedBugInfo> bugInfos) {
+//        for (ReportedBugInfo bug : bugInfos) {
+//            judge(bug);
+//        }
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "\n" +
+//                "Name: " + this.getName() + "\n" +
+//                "Bug Number: " + (this.getFp().size() + this.getTp().size()) + "\n" +
+//                "TP: " + this.getTp().size() + "\n" +
+//                "FP: " + this.getFp().size() + "\n" +
+//                "precision: " + this.getPrecision() + "\n" +
+//                "recall: " + this.getRecall() + "\n";
+//    }
+//
+//    public void printTags() {
+//        for (String key : tags.keySet()) {
+//            System.out.println(key + ":" + tags.get(key));
+//        }
+//    }
+//
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public double getPrecision() {
+//        return ((double) tp.size()) / (tp.size() + fp.size());
+//    }
+//
+//    public double getRecall() {
+//        return ((double) tp.size()) / CsvParser.FAKEBUGS.size();
+//    }
+//
+//    public Map<String, List<ReportedBugInfo>> getTp() {
+//        return tp;
+//    }
+//
+//    public Map<String, List<ReportedBugInfo>> getFp() {
+//        return fp;
+//    }
+//
+//    public Map<String, Integer> getTags() {
+//        return tags;
+//    }
+//
+//   /* public void countTags() {
+//        for (String uid : tp.keySet())
+//            for (String tag : CsvParser.FAKEBUGS.get(uid)) {
+//                if (!tags.containsKey(tag)) {
+//                    tags.put(tag, 1);
+//                } else {
+//                    tags.put(tag, tags.get(tag) + 1);
+//                }
+//            }
+//    }/
+//}
