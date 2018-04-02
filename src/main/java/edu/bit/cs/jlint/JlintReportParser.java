3f8c2b0e5b2fd9a88d7d104f9c3fab71bbbe5db5
aminu241 <aminu241@rocketmail.com>
2018-04-02 19:48:32 +0800

    Jlint report classes

--- src/main/java/edu/bit/cs/jlint/JlintReportParser.java
+++ src/main/java/edu/bit/cs/jlint/JlintReportParser.java
@@ -8,86 +8,90 @@ import edu.bit.cs.ReportedInfoProcessor;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class JlintReportParser implements ReportedInfoProcessor {
 
-    private ArrayList ReportedBugs = new ArrayList();
-
-    @Override
-    public Collection<? extends ReportedBugInfo> getReportedBugs(BufferedReader br) {
-
+    public static Collection<? extends ReportedBugInfo> getReportedBugs() {
+        ArrayList ReportedBugs = new ArrayList();
         try {
-            // create a Buffered Reader object instance with a FileReader, remeber to change the way the file is read as a resource
-//            BufferedReader br = new BufferedReader(new InputStreamReader(JlintReportParser.class.getClassLoader().getResourceAsStream("file/jlint_npe_repoter.txt")));
-            // read the first line from the text file
+            BufferedReader br = new BufferedReader(new InputStreamReader(JlintReportParser.class.getClassLoader().getResourceAsStream("file/jlint.txt")));
             String fileRead = br.readLine();
-            // loop until all lines are read
             while (fileRead != null) {
-
                 if (fileRead.contains("Verification completed")) {
                     fileRead = br.readLine();
                     continue;
                 }
-
-//                System.out.println("jlint report bug:" + fileRead);
-
                 int index;
                 if (fileRead.charAt(1) == ':') {
-                    //find second occurence of :, the number after that is the line number of the reported bug
                     index = fileRead.indexOf(":", fileRead.indexOf(":") + 1);
                 } else {
                     index = fileRead.indexOf(":");
                 }
-//                //find second occurence of :, the number after that is the line number of the reported bug
-//                int index = fileRead.indexOf(":", fileRead.indexOf(":") + 1);
                 if (index > -1) {
-                    //class path starts from the begining to just before the line number that a bug was reported
                     String classPath = fileRead.substring(0, index);
-//                    int start_index = classPath.lastIndexOf(root);
-//                    classPath = classPath.substring(start_index + root.length() + 1, index).replace("\\", "/");
-//                    System.out.println("ClassPath: " + classPath);
-
-                    //starts after the class path, they are separated by the index containing :
                     int startIndex_bugLineNumber = index + 1;
                     int endIndex_bugLineNumber = fileRead.indexOf(':', startIndex_bugLineNumber);
-//                    System.out.println("startIndex:" + startIndex_bugLineNumber + " endIndex:" + endIndex_bugLineNumber);
                     String bugLineNumber = fileRead.substring(startIndex_bugLineNumber, endIndex_bugLineNumber);
-
-//                    System.out.println("errorLineNumber: " + bugLineNumber);
-
-                    //message starts immediately after the line number
                     String errorMessage = fileRead.substring(endIndex_bugLineNumber + 1, fileRead.length()).trim();
-//                    System.out.println("Error Message: " + errorMessage);
-
-                    //Bug Type
-                    String bugType = getBugType(errorMessage);
-//                    System.out.println("Bug Type: " + getClassName(bugType));
-
-                    //Class Name
-//                    System.out.println("className: " + getClassName(classPath));
                     String className = getClassName(classPath);
-
-                    //sourceFile and ClassFile
                     String sourcePath = classPath;
-//                    System.out.println("--------------------------------------");
-                    JlintReportedBug bugInstance = new JlintReportedBug(bugType, errorMessage, className, Integer.valueOf(bugLineNumber), sourcePath);
+                    JlintReportedBug bugInstance = new JlintReportedBug(errorMessage, className, Integer.valueOf(bugLineNumber), sourcePath);
                     ReportedBugs.add(bugInstance);
-
                 }
-
                 fileRead = br.readLine();
             }
-
-            // close file stream
             br.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return ReportedBugs;
+    }
 
+    @Override
+    public Collection<? extends ReportedBugInfo> getReportedBugs(BufferedReader br) {
+        ArrayList ReportedBugs = new ArrayList();
+        try {
+            String fileRead = br.readLine();
+            String errorMessage;
+            String className;
+            String sourcePath;
+            while (fileRead != null) {
+
+                if (fileRead.contains("Verification completed")) {
+                    fileRead = br.readLine();
+                    continue;
+                }
+                int index;
+                if (fileRead.charAt(1) == ':') {
+                    index = fileRead.indexOf(":", fileRead.indexOf(":") + 1);
+                } else {
+                    index = fileRead.indexOf(":");
+                }
+                if (index > -1) {
+                    String classPath = fileRead.substring(0, index);
+                    int startIndex_bugLineNumber = index + 1;
+                    int endIndex_bugLineNumber = fileRead.indexOf(':', startIndex_bugLineNumber);
+                    String bugLineNumber = fileRead.substring(startIndex_bugLineNumber, endIndex_bugLineNumber);
+
+                    errorMessage = fileRead.substring(endIndex_bugLineNumber + 1, fileRead.length()).trim();
+                    className = getClassName(classPath);
+                    sourcePath = classPath;
+                    JlintReportedBug bugInstance = new JlintReportedBug(errorMessage, className, Integer.valueOf(bugLineNumber), sourcePath);
+                    ReportedBugs.add(bugInstance);
+                }
+                fileRead = br.readLine();
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return ReportedBugs;
     }
 
@@ -99,19 +103,6 @@ public class JlintReportParser implements ReportedInfoProcessor {
         return className;
 
     }
-
-    private static String getBugType(String errorMessage) {
-
-        //Exeption Type
-        BUG_TYPE bugType;
-        if (errorMessage.contains("NULL") || errorMessage.contains("null")) {
-            bugType = BUG_TYPE.NULL_POINTER_EXEPTION;
-        } else {
-            bugType = BUG_TYPE.ANOTHER_TYPE;
-        }
-        return bugType.toString();
-    }
-
 
 }
 
--- src/main/java/edu/bit/cs/jlint/JlintReportedBug.java
+++ src/main/java/edu/bit/cs/jlint/JlintReportedBug.java
@@ -14,16 +14,15 @@ import java.io.Reader;
 
 
 public class JlintReportedBug implements ReportedBugInfo {
-    private final String type;
-    private static String message;
+
+    private final String message;
     private final String className;
     private final int bugLineNumber;
     private final String sourcePath;
 
     public static final ToolCollection JLINT = ToolCollection.JLINT;
 
-    public JlintReportedBug(String type, String message, String className, int bugLineNumber, String sourcePath) {
-        this.type = type;
+    public JlintReportedBug(String message, String className, int bugLineNumber, String sourcePath) {
         this.message = message;
         this.className = className;
         this.bugLineNumber = bugLineNumber;
@@ -32,53 +31,55 @@ public class JlintReportedBug implements ReportedBugInfo {
 
     @Override
     public BUG_TYPE getBugType() {
-        if(is_Jlint_INHERIT()){
+        if(is_Jlint_INHERIT(message)){
             return BUG_TYPE.INHERITANCE;
-        }else if(is_Jlint_NPE()){
+        }else if(is_Jlint_NPE(message)){
             return BUG_TYPE.NULL_POINTER_EXEPTION;
-        }else if(is_Jlint_SYNC()){
+        }else if(is_Jlint_SYNC(message)){
             return BUG_TYPE.SYNCHRONIZATION;
         }else{
             return BUG_TYPE.ANOTHER_TYPE;
         }
     }
 
-    public static Boolean is_Jlint_NPE(){
-        if(message.contains(" can be invoked with NULL as number parameter and this parameter is used without check for null")){
+    public static Boolean is_Jlint_NPE(String message){
+        if(message.contains("can be invoked with NULL as number parameter and this parameter is used without check for null")){
             return true;
-        }else if(message.contains("Value of referenced ") && message.contains(" may be NULL")){
+        }else if(message.contains("Value of referenced ") && message.contains("may be NULL")){
             return true;
         }else if(message.contains("NULL reference can be used")){
             return true;
         }else if(message.contains("Shift count range [min,max] is out of domain")){
             return true;
+        }else if(message.contains("NULL")){
+            return true;
         }
         return false;
     }
 
-    public  static Boolean is_Jlint_SYNC(){
+    public static Boolean is_Jlint_SYNC(String message){
 
         if(message.contains("invocation of synchronized ") && message.contains(" can cause deadlock")){
             return true;
         }else if(message.contains("invocation of ")&&message.contains(" forms the loop in class dependency graph")){
             return true;
-        }else if(message.contains(" is requested while holding") || message.contains(" with other thread holding ") && message.contains(" and requesting")){
+        }else if(message.contains("is requested while holding") && message.contains("with other thread holding") && message.contains(" and requesting")){
             return true;
         }else if(message.contains("Method wait() can be invoked with monitor of other object locked")){
             return true;
-        }else if(message.contains("Call sequence to ") || message.contains(" can cause deadlock in wait()")){
+        }else if(message.contains("Call sequence to ") && message.contains(" can cause deadlock in wait()")){
             return true;
         }else if(message.contains("Synchronized") && message.contains(" is overridden by non-synchronized method of derived ")){
             return true;
-        }else if(message.contains(" can be called from different threads and is not synchronized")){
+        }else if(message.contains("can be called from different threads and is not synchronized")){
             return true;
         }else if(message.contains("Field name of class")){
             return true;
-        }else if(message.contains(" implementing ’Runnable’ interface is not \u2028synchronized")){
+        }else if(message.contains(" interface is not \u2028synchronized")){
             return true;
-        }else if(message.contains("Value of ") || message.contains(" is changed outside synchronization or constructor")){
+        }else if(message.contains("Value of ") && message.contains(" is changed outside synchronization or constructor")){
             return true;
-        }else if(message.contains("Value of ") || message.contains(" is changed while (potentially) owning it")){
+        }else if(message.contains("Value of ") && message.contains(" is changed while (potentially) owning it")){
             return true;
         }else if(message.contains(".wait() is called without synchronizing on ")){
             return true;
@@ -86,12 +87,12 @@ public class JlintReportedBug implements ReportedBugInfo {
         return false;
     }
 
-    public static Boolean is_Jlint_INHERIT(){
+    public static  Boolean is_Jlint_INHERIT(String message){
        if(message.contains("is not overridden by method with the same name of derived ")){
            return true;
-       }else if(message.contains("Component name in class name shadows one in base class name")){
+       }else if(message.contains("shadows one in base")){
            return true;
-       }else if(message.contains(" shadows component of class name")){
+       }else if(message.contains("shadows component of")){
            return true;
        }else if(message.contains("Method finalize() doesn’t call super.finalize()")){
            return true;
@@ -113,12 +114,12 @@ public class JlintReportedBug implements ReportedBugInfo {
     }
 
     public String getSourcePath() {
-        return ReportedBugInfo.normalizeFilePath(sourcePath, FindbugsSensor.ROOT);
+        return sourcePath;
     }
 
     @Override
     public ToolCollection getToolName() {
-        return JLINT;
+        return ToolCollection.JLINT;
     }
 
     @Override
@@ -132,31 +133,4 @@ public class JlintReportedBug implements ReportedBugInfo {
 
         return string.toString();
     }
-/*
-    public static void main(String[] args) {
-        System.out.println("SYNC");
-        System.out.println(is_Jlint_SYNC("invocation of synchronized method name can cause deadlock"));
-        System.out.println(is_Jlint_SYNC("invocation of method name forms the loop in class dependency graph"));
-        System.out.println(is_Jlint_SYNC("Lock a is requested while holding lock b, with other thread holding a and requesting lock b"));
-        System.out.println(is_Jlint_SYNC("Method wait() can be invoked with monitor of other object locked"));
-        System.out.println(is_Jlint_SYNC("Call sequence to method name can cause deadlock in wait()"));
-        System.out.println(is_Jlint_SYNC(" Synchronized method name is overridden by non-synchronized method of derived class name"));
-        System.out.println(is_Jlint_SYNC("Method name can be called from different threads and is not synchronized"));
-        System.out.println(is_Jlint_SYNC("Field name of class"));
-        System.out.println(is_Jlint_SYNC("Method name implementing ’Runnable’ interface is not  synchronized"));
-        System.out.println(is_Jlint_SYNC("Value of lock name is changed outside synchronization or constructor"));
-        System.out.println(is_Jlint_SYNC("Value of lock name is changed while (potentially) owning it"));
-        System.out.println(is_Jlint_SYNC(" Method name.wait() is called without synchronizing on name"));
-        System.out.println("NPE");
-        System.out.println(is_Jlint_NPE("Method name can be invoked with NULL as number parameter and this parameter is used without check for null"));
-        System.out.println(is_Jlint_NPE("Value of referenced variable name may be NULL"));
-        System.out.println(is_Jlint_NPE("NULL reference can be used"));
-        System.out.println(is_Jlint_NPE("Shift count range [min,max] is out of domain"));
-        System.out.println("INHERIT");
-        System.out.println(is_Jlint_INHERIT("Method name is not overridden by method with the same name of derived class name"));
-        System.out.println(is_Jlint_INHERIT("Component name in class name shadows one in base class name"));
-        System.out.println(is_Jlint_INHERIT("Local variable name shadows component of class name"));
-        System.out.println(is_Jlint_INHERIT("Method finalize() doesn’t call super.finalize()"));
-    }
-    */
 }
